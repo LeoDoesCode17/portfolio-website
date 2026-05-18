e59f6710b56f603dbc960bbd97f5336cba324dab
leonardo <leocode1079@gmail.com>
2026-05-18 13:28:20 +0800

    feat: add cors configuration

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,8 +1,19 @@
 # /fastapi/app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import image, auth, user
+from app.core.config import settings
 
 app = FastAPI()
+
+# CORS CONFIGURATION
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def read_root():
