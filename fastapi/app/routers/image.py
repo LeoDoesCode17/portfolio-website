49a5a39fa5e481f0c6b9cb6263dcb3fd7256de48
leonardo <leocode1079@gmail.com>
2026-05-22 14:24:49 +0800

    feat: use repositories to get all images

--- fastapi/app/routers/image.py
+++ fastapi/app/routers/image.py
@@ -1,6 +1,9 @@
 # fastapi/app/routers/image.py
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
 from app.schemas.image import ImageResponse, ImageCreate
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+from app.repositories import image as repository
 
 router = APIRouter(
     prefix='/images',
@@ -8,15 +11,9 @@ router = APIRouter(
 )
 
 @router.get('/', response_model=list[ImageResponse])
-async def read_all_images():
-    return [
-        {
-            'id': 1,
-            'name': 'Leonardo Image',
-            'url': 'Leonardo-ulr.url',
-            'alt_text': 'This is the alt text'
-        }
-    ]
+async def read_all_images(db: Session = Depends(get_db)):
+    images = repository.get(db)
+    return images
 
 @router.post('/', response_model=ImageResponse)
 async def create_an_image(image: ImageCreate):
