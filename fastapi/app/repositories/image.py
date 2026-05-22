f900bc2d3438f77a1b25a6db955f5abeab54bdb8
leonardo <leocode1079@gmail.com>
2026-05-22 15:01:39 +0800

    feat: create function to update an image

--- fastapi/app/repositories/image.py
+++ fastapi/app/repositories/image.py
@@ -31,3 +31,28 @@ def create(db: Session, data: dict) -> Image:
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Failed to create image"
         )
+    
+def update(db: Session, id: int, data: dict) -> Image:
+    image_instance = db.query(Image).filter(
+        Image.id == id,
+        Image.is_deleted == False
+    ).first()
+
+    if not image_instance:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Image with id {id} not found"
+        )
+
+    try:
+        for key, val in data.items():
+            setattr(image_instance, key, val)
+        db.commit()
+        db.refresh(image_instance)
+        return image_instance
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to update image with id {id}"
+        )
