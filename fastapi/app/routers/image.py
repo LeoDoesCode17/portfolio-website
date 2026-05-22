9742ba55675bbdfcbd60e7c31dfc9d08ace10591
leonardo <leocode1079@gmail.com>
2026-05-22 14:46:18 +0800

    feat: use create image function from repository

--- fastapi/app/routers/image.py
+++ fastapi/app/routers/image.py
@@ -1,8 +1,10 @@
-# fastapi/app/routers/image.py
-from fastapi import APIRouter, Depends, status, HTTPException
-from app.schemas.image import ImageResponse, ImageCreate
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from app.core.database import get_db
+from app.schemas.user import UserResponse
+from typing import Annotated
+from app.auth import get_current_user
+from app.schemas.image import ImageResponse, ImageCreate
 from app.repositories import image as repository
 
 router = APIRouter(
@@ -11,17 +13,13 @@ router = APIRouter(
 )
 
 @router.get('/', response_model=list[ImageResponse])
-async def read_all_images(db: Session = Depends(get_db)):
-    images = repository.get(db)
-    return images
+def read_all_images(db: Session = Depends(get_db)):
+    return repository.get(db)
 
-@router.post('/', response_model=ImageResponse)
-async def create_an_image(image: ImageCreate):
-    return {
-        'id': 1,
-        'name': image.name,
-        'url': image.url,
-        'alt_text': image.alt_text
-    }
-
-
+@router.post('/', response_model=ImageResponse, status_code=status.HTTP_201_CREATED)
+def create_an_image(
+    image: ImageCreate,
+    current_user: Annotated[UserResponse, Depends(get_current_user)],
+    db: Session = Depends(get_db)
+):
+    return repository.create(db, image.model_dump())
