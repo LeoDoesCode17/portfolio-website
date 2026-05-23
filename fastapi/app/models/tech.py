09a82dfaf4236bc119b2cc6b84a592fe30316744
leonardo <leocode1079@gmail.com>
2026-05-23 20:28:58 +0800

    feat: define relationship between techs and post_techs

--- fastapi/app/models/tech.py
+++ fastapi/app/models/tech.py
@@ -2,7 +2,11 @@
 
 from app.core.database import Base
 from sqlalchemy import Integer, String, Boolean, Text
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.models.post_tech import PostTech
 
 class Tech(Base):
     __tablename__ = 'techs'
@@ -10,3 +14,10 @@ class Tech(Base):
     name: Mapped[str] = mapped_column(Text, nullable=False)
     slug: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
     is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+    # Relationship
+    post_techs: Mapped[list["PostTech"]] = relationship(
+        "PostTech",
+        back_populates="tech",
+        primaryjoin="and_(Tech.id == PostTech.tech_id, PostTech.is_deleted == False)"
+    )
