0c67949f511310fe2ef5e9a5625193adaee446f1
leonardo <leocode1079@gmail.com>
2026-05-27 14:18:22 +0800

    feat: include post router in main

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,7 +1,7 @@
 # /fastapi/app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import image, auth, user, tech
+from app.routers import image, auth, user, tech, post
 from app.core.config import settings
 
 app = FastAPI()
@@ -27,4 +27,5 @@ app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(image.router)
 app.include_router(tech.router)
+app.include_router(post.router)
 
