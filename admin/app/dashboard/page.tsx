c9727f6df9c2719e28abf8358368b15d31d372a4
leonardo <leocode1079@gmail.com>
2026-05-29 14:15:59 +0800

    feat: remove get auth tokens  from dashboard page

--- admin/app/dashboard/page.tsx
+++ admin/app/dashboard/page.tsx
@@ -1,21 +1,7 @@
-// app/dashbaord/page.tsx
+// app/dashboard/page.tsx
 
-import { getAuthToken } from "@/lib/auth";
-import { redirect } from "next/navigation";
 import PostListSection from "./components/PostListSection";
 
-export default async function DashboardPage() {
-    const token = await getAuthToken();
-    if(!token) {
-        redirect('/login');
-    }
-
-  return (
-    <main className="min-h-screen bg-slate-950 text-slate-100">
-      <div className="mx-auto max-w-4xl px-6 py-8">
-        <h1 className="text-xl font-semibold tracking-tight">Dashboard</h1>
-        <PostListSection/>
-      </div>
-    </main>
-  );    
+export default function DashboardPage() {
+  return <PostListSection />;
 }
