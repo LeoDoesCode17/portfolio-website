8c3397857c6f31b9cf46990c3f684328444b1870
leonardo <leocode1079@gmail.com>
2026-06-02 19:33:12 +0800

    feat: format file using prettier

--- admin/app/dashboard/posts/[slug]/page.tsx
+++ admin/app/dashboard/posts/[slug]/page.tsx
@@ -2,7 +2,6 @@
 import PostForm from "@/app/dashboard/components/PostForm";
 import type { Post } from "@/types/post";
 
-
 const LOCAL_DEV_URL = "http://127.0.0.1:8000";
 const FASTAPI_URL = process.env.API_URL ?? LOCAL_DEV_URL;
 
@@ -21,30 +20,28 @@ async function getPostBySlug(slug: string): Promise<Post | null> {
   return data;
 }
 
-
 export default async function EditPostPage({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-    const slug = (await params).slug
-    const post = await getPostBySlug(slug);
+  const slug = (await params).slug;
+  const post = await getPostBySlug(slug);
 
-    if (!post) {
-        return (
-            <div className="mt-4 rounded-md border border-red-500/40 bg-red-950/60 px-3 py-2 text-xs text-red-100">
-                Post with slug <span className="font-mono">{slug}</span> not
-                found.
-            </div>
-        );
-    }
-
+  if (!post) {
     return (
-        <div>
-            <h2 className="text-sm font-semibold text-slate-300">
-                Edit post: <span className="font-mono text-slate-100">{post.slug}</span>
-            </h2>
-            <PostForm mode="edit" initialPost={post} />
-        </div>
+      <div className="mt-4 rounded-md border border-red-500/40 bg-red-950/60 px-3 py-2 text-xs text-red-100">
+        Post with slug <span className="font-mono">{slug}</span> not found.
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <h2 className="text-sm font-semibold text-slate-300">
+        Edit post: <span className="font-mono text-slate-100">{post.slug}</span>
+      </h2>
+      <PostForm mode="edit" initialPost={post} />
+    </div>
+  );
 }
